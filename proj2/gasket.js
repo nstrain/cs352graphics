bd0497218c3846747a61d774b48daea81b8cea88
nstrain <nstrain15@gmail.com>
2022-01-27 19:18:37 -0500

    invert fatness

--- proj2/gasket.js
+++ proj2/gasket.js
@@ -123,7 +123,7 @@ gasket.slider = function (ev) {
 
 gasket.fatAngle = function() {
   fattness = $('#slider1').val();
-  return (fattness) * (Math.PI/180);
+  return (80-fattness) * (Math.PI/180);
 }
 
 //arm length 0.35355339059

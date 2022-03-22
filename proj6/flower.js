be7da02f3add2b28d0a4854f386c8ca8a97a7e62
nstrain <nstrain15@gmail.com>
2022-03-22 17:41:27 -0400

    rotating petals

--- proj6/flower.js
+++ proj6/flower.js
@@ -4,7 +4,11 @@ import { TrackballControls } from "./js/TrackballControls.js";
 $(document).ready(function () { flower.init(); });
 
 var flower = {
-    petals: 2,
+    numPetals: 10,
+    petals: [],
+    petalGeom: [],
+    petalLocation: [],
+    petalWidth: Math.PI / 3
 };
 
 flower.init = function () {
@@ -18,14 +22,34 @@ flower.init = function () {
     flower.controls = new TrackballControls(flower.camera, flower.renderer.domElement);
     flower.controls.rotateSpeed = 5.0;
     flower.controls.zoomSpeed = 1.2;
-    flower.controls.panSpeed = 5.0;
+    flower.controls.panSpeed = 1.0;
     flower.controls.keys = ['KeyA', 'KeyS', 'KeyD'];
 
 
-    const geometry = new THREE.ConeGeometry(0.09, 2.8, 8);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var geometry = new THREE.ConeGeometry(0.09, 2.8, 8);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     flower.stem = new THREE.Mesh(geometry, material);
     flower.scene.add(flower.stem);
+
+    var points = [];
+    for (let i = 0; i < 10; i++) {
+        points.push(new THREE.Vector2(Math.sin(i * 0.2) * 0.5 + 0.5, (i) * 1 / 10));
+    }
+
+    for (let i = 0; i < flower.numPetals; i++) {
+        this.petalLocation.push((i * (flower.petalWidth / 2)) % (2*Math.PI));
+    }
+    for(let i = 0; i < flower.numPetals; i++) {
+        flower.petalGeom.push(new THREE.LatheGeometry(points, 12, 1, flower.petalWidth));
+    }
+    material = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
+    var lathe = new THREE.Mesh(geometry, material);
+    // flower.scene.add(flower.lathe);
+    for (let i = 0; i < flower.numPetals; i++) {
+        flower.petals.push(lathe);
+    }
+
+    flower.scene.add(flower.petals[0]);
 
     flower.camera.position.z = 5;
     animate();
@@ -38,6 +62,23 @@ function animate() {
     // flower.cube.rotation.y += 0.01;
     // flower.petal(1,1,1);
     flower.controls.update();
+
+    var material = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
+    for (let i = 0; i < 1; i++) {
+        var points = [];
+        for (let i = 0; i < 10; i++) {
+            points.push(new THREE.Vector2(Math.sin(i * 0.2) * 0.5 + 0.5, (i) * 1 / 10));
+        }
+        flower.scene.remove(flower.petals[i]);
+        flower.petalGeom[i].dispose();
+        flower.petalGeom[i] = new THREE.LatheGeometry(points, 12, flower.petalLocation[i], flower.petalWidth);
+        flower.petalLocation[i] = (flower.petalLocation[i] + flower.petalWidth / 50) % (2*Math.PI);
+        flower.petals[0] = new THREE.Mesh(
+            flower.petalGeom[i],
+            material);
+        flower.scene.add(flower.petals[0]);
+    }
+
 
     flower.renderer.render(flower.scene, flower.camera);
 };
